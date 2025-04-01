f77b8019aaf1ecea229e57eba9559a34e4269cfa
Công Minh Hoàng <420ent.@Congs-MacBook-Air.local>
2025-04-01 11:57:47 +0700

    route (/) completed

--- Backend/routes/pokemon.js
+++ Backend/routes/pokemon.js
@@ -1,7 +1,9 @@
+const { getDefaultResultOrder } = require("dns");
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 const path = require("path");
+const { z } = require("zod");
 
 const dbFile = path.resolve(__dirname, "../db.json");
 
@@ -30,7 +32,24 @@ router.get("/", async (req, res, next) => {
     const fileData = JSON.parse(fs.readFileSync(dbFile, "utf8"));
     let pokemons = fileData.data;
 
-    let { name, type, limit, page } = req.query;
+    //define schema for validation
+    const requestQuerySchema = z.object({
+      name: z.string().optional(),
+      type: z.enum(pokemonTypes).optional(),
+      limit: z.coerce.number().int().min(1).max(100).default(10),
+      page: z.coerce.number().int().min(1).default(1),
+    });
+
+    // validate by defined schema + safeParse (not throw error by default --> be able to customize how to handle error)
+    const result = requestQuerySchema.safeParse(req.query);
+    //handle error
+    if (!result.success) {
+      return res.status(400).json({
+        message: "BAD REQUEST",
+      });
+    }
+
+    let { name, type, limit, page } = result.data;
 
     if (name) {
       name = name.toLowerCase();
@@ -43,40 +62,9 @@ router.get("/", async (req, res, next) => {
         p.types.some((t) => t.toLowerCase() === type)
       );
     }
-    //add request validation Joi Validator Zod validator
-    // const requestQuerySchema = z.object({
-    //    name: z.string().optional(),
-    //    type: z.enum(pokemonTypes).optional(),
-    //    limit: z.number().optional().default(10).max(100),
-    //    page: z.number().optional().default(1),
-    // });
-    //
-    //  const { name, type, limit, page } = requestQuerySchema.parse(req.query)
-    // nếu req.query = {} => parse sẽ trả về { limit: 10, page: 1}
-    // nếu req.quer = { type: "hello" } => parse sẽ throw error
-    //
-    // Treat các lỗi do request validation (ở đây sử dụng zod) trả về lỗi BAD REQUEST - 400
 
-    // Check lại các lỗi HTTP ERROR:
-    // - 400
-    // - 401
-    // - 403
-    // - 404
-    // - 409
-    // - 429
-    // - 500
-    // - 503
-    // - 501
-    // 5xx khác gì với 4xx
     limit = limit ? parseInt(limit, 10) : 10;
     page = page ? parseInt(page, 10) : 1;
-
-    if (isNaN(limit) || limit < 1) {
-      return res.status(400).json({ error: "Invalid limit value" });
-    }
-    if (isNaN(page) || page < 1) {
-      return res.status(400).json({ error: "Invalid page value" });
-    }
 
     const startIndex = (page - 1) * limit;
     const endIndex = startIndex + limit;
